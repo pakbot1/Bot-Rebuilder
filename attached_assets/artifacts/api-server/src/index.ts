1d1574dc4cbaff594608db517c5cb261bbdab151
Faizyy <faizulhadidhedhi@gmail.com>
2026-03-30 21:28:17 +0500

    fixxing prompts

--- attached_assets/artifacts/api-server/src/index.ts
+++ attached_assets/artifacts/api-server/src/index.ts
@@ -22,11 +22,10 @@ if (Number.isNaN(port) || port <= 0) {
 }
 
 // Serve static frontend files
-app.use(express.static(path.join(__dirname, '../../../pak-bot/dist')));
+app.use(express.static(path.join(__dirname, '../../../pak-bot/dist/public')));
 
-// Catch-all route for React SPA
 app.get('/{*splat}', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../pak-bot/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../../pak-bot/dist/public/index.html'));
 });
 
 app.listen(port, (err) => {
